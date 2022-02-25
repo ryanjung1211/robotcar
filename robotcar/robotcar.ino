61708983cf0bb8537962081158d9722fe8ebe095
Ryan Jung <jungrc@bc.edu>
2022-02-25 11:42:53 -0500

    part 2

--- robotcar/robotcar.ino
+++ robotcar/robotcar.ino
@@ -4,6 +4,72 @@
 #define IN2 8
 #define IN3 9
 #define IN4 11
+#define sp 100
+
+#include<Servo.h>
+Servo servo;
+
+void setup(){
+  pinMode(A5, OUTPUT);
+  pinMode(A4, INPUT);
+  Serial.begin(9600);
+  servo.attach(3);
+}
+
+void obstacle(){
+  float d = getDistance();
+  if (d < 15) {
+    stop();
+    servo.write(0);
+    float a = getDistance();
+    if (a > 15) {
+      turnLeft();
+      stop();
+      servo.write(90);
+    }
+    else{
+      delay(1000);
+      servo.write(180);
+      float c = getDistance();
+      if (c > 15) {
+        turnRight();
+        stop();
+        servo.write(90);
+      }
+      else {
+        turnRight();
+        turnRight();
+        stop();
+
+        servo.write(90);
+      }
+      }
+    }
+    
+    delay(1000);
+    servo.write(90);
+}
+
+double getDistance(){
+  digitalWrite(A5, LOW);
+  delayMicroseconds(2);
+  digitalWrite(A5, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(A5, LOW);
+  
+  long raw = pulseIn(A4, HIGH);
+  double distance = raw * 0.034 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println("cm");
+  
+  return distance;
+}
+void stop(){
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+}
 void moveForward(){
   analogWrite(ENA, 150);
   analogWrite(ENB, 150);
@@ -48,14 +114,9 @@ void turnRight(){
   digitalWrite(IN4, LOW);
 }
 
-void stop(){
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
-}
 
-void setup(){
-}
+
 void loop(){
-  moveForward()
-  move
+  obstacle();
+  delay(1000);
 }
